cf6eeba39820df8d3ece574e9858211e58522da7
EAGLEEARTH <ufukcicek199@gmail.com>
2023-09-30 22:02:10 +0300

    sqlqueries add order by ıd added

--- adin_queries.py
+++ adin_queries.py
@@ -1,9 +1,9 @@
  
 # sql_queries.py
-SELECT_ALL_DOMAINS_DATA = "SELECT * FROM adin_telecommunication_product_detail;"
-SELECT_DATA_BY_DATE = "SELECT * FROM adin_telecommunication_product_detail WHERE create_date BETWEEN %s AND %s AND domain_id = %s;"
-SELECT_DATA_BY_CREATEDATE = "SELECT * FROM adin_telecommunication_product_detail WHERE create_date = %s"
-SELECT_DATA_BY_CREATEDATE_DOMAINID = "SELECT * FROM adin_telecommunication_product_detail WHERE create_date = %s and domain_id = %s;"
+SELECT_ALL_DOMAINS_DATA = "SELECT * FROM adin_telecommunication_product_detail order by Id;"
+SELECT_DATA_BY_DATE = "SELECT * FROM adin_telecommunication_product_detail WHERE create_date BETWEEN %s AND %s AND domain_id = %s order by Id;"
+SELECT_DATA_BY_CREATEDATE = "SELECT * FROM adin_telecommunication_product_detail WHERE create_date = %s order by Id;"
+SELECT_DATA_BY_CREATEDATE_DOMAINID = "SELECT * FROM adin_telecommunication_product_detail WHERE create_date = %s and domain_id = %s order by Id;"
 
 
 
